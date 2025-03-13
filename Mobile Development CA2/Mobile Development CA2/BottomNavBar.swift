4c7662104f93e29bd6190d19dd26370153e321b1
Philip Philip <102297752+v-philip@users.noreply.github.com>
2025-03-13 13:34:05 +0000

    Create BottomNavBar.swift

---
+++ Mobile Development CA2/Mobile Development CA2/BottomNavBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomNavBar.swift
+//  Mobile Development CA2
+//
+//  Created by Student on 13/03/2025.
+//
+
+import SwiftUI
+
+struct BottomNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomNavBar()
+}
