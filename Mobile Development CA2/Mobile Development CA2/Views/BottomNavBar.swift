16817a76d244d31a913d61e5d399bc99bb8aace3
Philip Philip <102297752+v-philip@users.noreply.github.com>
2025-03-13 14:10:01 +0000

    bottom bar navigation
    bottom bar navigation without animation

---
+++ Mobile Development CA2/Mobile Development CA2/Views/BottomNavBar.swift
@@ -0,0 +1,67 @@
+//
+//  BottomNavBar.swift
+//  Mobile Development CA2
+//
+//  Created by Student on 13/03/2025.
+//
+
+import SwiftUI
+
+struct BottomNavBar: View {
+    var body: some View {
+        ZStack {
+            // Background shape
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.quaternary) // Adjust color as needed
+                .frame(height: 80)
+                .padding(.horizontal, 20)
+                .shadow(radius: 5)
+
+            HStack(spacing: 40) {
+                Button(action: {
+                    // Action for first button
+                }) {
+                    Image(systemName: "briefcase.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                }
+                
+                ZStack {
+                    Capsule()
+                        .fill(Color.secondaryColor)
+                        .frame(width: 100, height: 45)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color.backgroundColor)
+                        Text("Search")
+                            .foregroundColor(Color.backgroundColor)
+                            .font(.system(size: 16, weight: .bold))
+                    }
+                }
+                
+                Button(action: {
+                    // Action for second button
+                }) {
+                    Image(systemName: "person.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                }
+                
+                Button(action: {
+                    // Action for third button
+                }) {
+                    Image(systemName: "bubble.left.and.bubble.right.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                }
+            }
+            .padding(.bottom, 10)
+        }
+    }
+}
+#Preview {
+    BottomNavBar()
+}
+
+
