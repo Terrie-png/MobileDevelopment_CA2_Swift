cb2e54ba0b2ace8993f00a9e53d32ed52c9b9952
Terrie-png <ongtan597@gmail.com>
2025-03-20 13:50:00 +0000

    Added Cards View for swipping with colors together when the gesture was dragging

---
+++ Mobile Development CA2/Mobile Development CA2/Views/Main Page/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  Mobile Development CA2
+//
+//  Created by Student on 20/03/2025.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
