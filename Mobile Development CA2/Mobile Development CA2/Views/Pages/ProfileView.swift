a12ba70b0751b6bd3f0746c58b2b2e19829275d2
Terrie-png <ongtan597@gmail.com>
2025-04-10 14:14:17 +0100

    Update ProfileView.swift

--- Mobile Development CA2/Mobile Development CA2/Views/Pages/ProfileView.swift
+++ Mobile Development CA2/Mobile Development CA2/Views/Pages/ProfileView.swift
@@ -3,57 +3,61 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack{
-            Color.primary.frame(height: 200)
-            CircleImageView()
-                            .frame(width: 200, height: 200)
-                            .offset(y: -130) .padding(.bottom, -130)
-            VStack() {
-//                NavigationLink(destination:test()){
-//                    HStack{
-//                        Image(systemName: "square.and.arrow.up")
-//                        Text("Upload Resume")
-//                    }
-//                }
+        ZStack{
+            Color.secondaryColor.ignoresSafeArea()
+            
+            VStack{
+                Color.primary.frame(height: 200)
+                CircleImageView()
+                                .frame(width: 200, height: 200)
+                                .offset(y: -130) .padding(.bottom, -130)
+                VStack() {
+    //                NavigationLink(destination:test()){
+    //                    HStack{
+    //                        Image(systemName: "square.and.arrow.up")
+    //                        Text("Upload Resume")
+    //                    }
+    //                }
 
-                Divider().background(Color.secondaryColor)
-                NavigationLink(destination:LocationSettingsView()){
-                    HStack{
-                        Image(systemName: "gearshape")
-                        Text("Settings")
-                    }
-                }
-                    Divider()
-                NavigationLink(destination:test()){
-                    HStack{
-                        Image(systemName: "accessibility")
-                        Text("Accessibility")
-                    }
-                }
-                    Divider()
-                Spacer()
-                    HStack(spacing: 15) {
-                        Button(action: {
-                            // Withdraw Application
-                        }) {
-                            Text("Log Out")
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .background(Color.red)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
+                    Divider().background(Color.secondaryColor)
+                    NavigationLink(destination:LocationSettingsView()){
+                        HStack{
+                            Image(systemName: "gearshape")
+                            Text("Settings")
                         }
                     }
-                    .padding(.top)
-                }
-                       }
-                       .padding()
+                        Divider()
+                    NavigationLink(destination:test()){
+                        HStack{
+                            Image(systemName: "accessibility")
+                            Text("Accessibility")
+                        }
+                    }
+                        Divider()
+                    Spacer()
+                        HStack(spacing: 15) {
+                            Button(action: {
+                                // Withdraw Application
+                            }) {
+                                Text("Log Out")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .background(Color.red)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                        }
+                        .padding(.top)
+                    }
+                           }
+                           .padding()
 
 
-                       Spacer()
+                           Spacer()
+                
             
-        
-        
+            
+        }
     }
 }
 
